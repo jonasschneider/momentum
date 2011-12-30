77edb537e7f1320b888e127fba06737174ad544e
Jonas Schneider <mail@jonasschneider.com>
2011-12-30 21:02:21 +0100

    add spec for chunked reponses

--- spec/momentum/adapters/accelerate/windigo_spec.rb
+++ spec/momentum/adapters/accelerate/windigo_spec.rb
@@ -4,12 +4,12 @@ require "momentum"
 include Momentum::Adapters
 
 describe Accelerate::Windigo do
-  let(:socket_file) { '/tmp/unicorn.sock' }
   let(:socket) do
     sock = nil
+    sleep 0.2
     Timeout.timeout(4) do
       begin
-        sock = UNIXSocket.new(socket_file)
+        sock = UNIXSocket.new(@socket_name)
       rescue Errno::ECONNREFUSED
         sleep 0.2
         retry
@@ -18,15 +18,19 @@ describe Accelerate::Windigo do
     sock
   end
 
-  let(:server) { Accelerate::Windigo.new(app, listeners: socket_file) }
+  let(:server) { Accelerate::Windigo.new(app, listeners: @socket_name) }
   
-  before do
+  before :each do
+    t = Tempfile.new('momentum-spec')
+    @socket_name = t.path
+    t.unlink
+    
     @pid = fork do
       server.start
     end
   end
   
-  after do
+  after :each do
     Process.kill(:TERM, @pid)
   end
   
@@ -41,6 +45,14 @@ describe Accelerate::Windigo do
     socket.write data
   end
   
+  def read_frame
+    type = socket.read(1).to_i
+
+    len = socket.read(4).unpack('L').first
+    body = socket.read(len)
+    { type: type, len: len, body: body }
+  end
+  
   context "response body" do
     let(:app) { lambda { |env| [200, {"Content-Type" => "text/plain"}, ['ohai']] } }
     
@@ -52,6 +64,21 @@ describe Accelerate::Windigo do
       len = socket.read(4).unpack('L').first
       body_data = socket.read(len)
       body_data.should == 'ohai'
+    end
+  end
+
+  context "chunked response body" do
+    let(:app) { lambda { |env| [200, {"Content-Type" => "text/plain"}, ['ohai', 'test']] } }
+
+    it "gets returned" do
+      send_request!
+      f = read_frame
+      f[:type].should == Accelerate::Windigo::BODY_CHUNK
+      f[:body].should == 'ohai'
+      
+      f = read_frame
+      f[:type].should == Accelerate::Windigo::BODY_CHUNK
+      f[:body].should == 'test'
     end
   end
 end
