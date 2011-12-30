61b8435cd8e1dd15c7183d2f44e25c274a524e12
Jonas Schneider <mail@jonasschneider.com>
2011-12-30 03:52:03 +0100

    remove contect

--- spec/support/backend_examples.rb
+++ spec/support/backend_examples.rb
@@ -1,81 +1,82 @@
 require "timeout"
 
 shared_examples "Momentum backend" do 
-  describe "after making a request" do
-    let(:app) { lambda { |env| [200, {"Content-Type" => "text/plain"}, [response]] } }
-    let(:response) { 'hello from my rack app' }
-    
-    let(:rack_env) { { "a" => "b" } }
-    
-    let(:headers) { { 'method' => 'get', 'version' => 'HTTP/1.1', 'url' => '/', 'host' => 'localhost', 'scheme' => 'http' } }
+  let(:app) { lambda { |env| [200, {"Content-Type" => "text/plain"}, [response]] } }
+  let(:response) { 'hello from my rack app' }
+  
+  let(:rack_env) { { "a" => "b" } }
+  
+  let(:headers) { { 'method' => 'get', 'version' => 'HTTP/1.1', 'url' => '/', 'host' => 'localhost', 'scheme' => 'http' } }
 
-    let(:request) { stub(
-      :headers => headers,
-      :uri => URI.parse('http://localhost/'),
-      :to_rack_env => rack_env
-    ) }
+  let(:request) { stub(
+    :headers => headers,
+    :uri => URI.parse('http://localhost/'),
+    :to_rack_env => rack_env
+  ) }
+  
+  let(:reply) { backend.prepare(request) }
+  
+  def dispatch!
+    reply.on_complete do
+      EM.stop
+    end
     
-    let(:reply) { backend.prepare(request) }
+    Timeout::timeout(4) {
+      EM.run do
+        reply.dispatch!
+      end
+    }
     
-    def dispatch!
-      reply.on_complete do
-        EM.stop
+  ensure
+    EM.stop
+  end
+  
+  def response_headers
+    {}.tap do |headers|
+      reply.on_headers do |h|
+        headers.merge!(h)
       end
       
-      Timeout::timeout(4) {
-        EM.run do
-          reply.dispatch!
-        end
-      }
+      dispatch!
     end
-    
-    def response_headers
-      {}.tap do |headers|
-        reply.on_headers do |h|
-          headers.merge!(h)
-        end
-        
-        dispatch!
+  end
+  
+  def response_body
+    ''.tap do |data|
+      reply.on_body do |c|
+        data << c
       end
-    end
-    
-    def response_body
-      ''.tap do |data|
-        reply.on_body do |c|
-          data << c
-        end
-        
-        dispatch!
-      end
-    end
-    
-    context "request headers" do
-      let(:app) { lambda { |env| [200, {"Content-Type" => "text/plain"}, [env['HTTP_ME_PRO'].inspect]] } }
-      let(:headers) { { :url => '/', 'Me-pro' => 'Yup' } }
-      let(:rack_env) { { "HTTP_ME_PRO" => 'Yup' } }
       
-      it "passes them on" do
-        response_body.should == '"Yup"'
-      end
+      dispatch!
     end
+  end
+  
+  context "request headers" do
+    let(:app) { lambda { |env| [200, {"Content-Type" => "text/plain"}, [env['HTTP_ME_PRO'].inspect]] } }
+    let(:headers) { { :url => '/', 'Me-pro' => 'Yup' } }
+    let(:rack_env) { { "HTTP_ME_PRO" => 'Yup' } }
+    
+    it "passes them on" do
+      response_body.should == '"Yup"'
+    end
+  end
 
-    context "response headers" do
-      let(:app) { lambda { |env| [200, {"Content-Type" => "text/plain", 'Me-Pro' => 'Yup'}, ['wayne']] } }
-      
-      it "passes them on" do
-        response_headers['me-pro'].should == 'Yup'
-      end
-      
-      it "sets :status and :version" do
-        response_headers['status'].should == '200'
-        response_headers['version'].should == 'HTTP/1.1'
-      end
+  context "response headers" do
+    let(:app) { lambda { |env| [200, {"Content-Type" => "text/plain", 'Me-Pro' => 'Yup'}, ['wayne']] } }
+    
+    it "passes them on" do
+      response_headers['me-pro'].should == 'Yup'
     end
+    
+    it "sets :status and :version" do
+      response_headers['status'].should == '200'
+      response_headers['version'].should == 'HTTP/1.1'
+    end
+  end
 
-    describe "#body" do
-      it "fetches the response body" do
-        response_body.should == response
-      end
+  describe "#body" do
+    it "fetches the response body" do
+      response_body.should == response
     end
   end
 end
