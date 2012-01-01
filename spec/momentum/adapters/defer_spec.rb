37a63afb064320d920cccd696e12558954a39857
Jonas Schneider <mail@jonasschneider.com>
2012-01-01 11:56:43 +0100

    add failing spec for running the push callback in the main thread

--- spec/momentum/adapters/defer_spec.rb
+++ spec/momentum/adapters/defer_spec.rb
@@ -5,12 +5,38 @@ require "momentum"
 require "rack"
 require 'webmock/rspec'
 
-describe Momentum::Adapters::Proxy do
-  let(:app) { lambda { |env| sleep 1; [given_response_status, given_response_headers, [given_response_body]] } }
+describe Momentum::Adapters::Defer do
+  let(:app) { lambda { |env| [given_response_status, given_response_headers, [given_response_body]] } }
   let(:backend) do
     a = Momentum::Adapters::Defer.new(app)
     Momentum::Backend.new(a)
   end
   
   include_examples "Momentum backend"
+  include_examples "Backend server push"
+  
+  context "push callback" do
+    let(:backend) do
+      a = Momentum::Adapters::Defer.new(app)
+      Momentum::Backend.new(a)
+    end
+
+    let(:app) { lambda { |env|
+      puts "pushing to #{env['spdy'].inspect}"
+      env['spdy'].push('/test.js')
+      [200, {}, []]
+    } }
+
+    it "gets called in the main thread" do
+      delegate = stub
+      ran_in_thread = nil
+      delegate.stub(:push) do |url| 
+        ran_in_thread = Thread.current
+      end
+      backend_response.stub(:build_delegate => delegate)
+      dispatch!
+      
+      ran_in_thread.should == Thread.current
+    end
+  end
 end
