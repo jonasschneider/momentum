a378c60e5e43bfc2e3d626a7baf8fcec7258fff4
Jonas Schneider <mail@jonasschneider.com>
2012-01-01 12:55:51 +0100

    implement cross-thread push queue with EM::Queue

--- lib/momentum/adapters/defer.rb
+++ lib/momentum/adapters/defer.rb
@@ -12,6 +12,7 @@ module Momentum
       
       def _call(env)
         @env = env
+        prepare!
         EM.defer method(:run_app), method(:send_response)
         
         throw(:async)
@@ -22,6 +23,24 @@ module Momentum
         r = @app.call(@env)
         #Momentum.logger.debug "[#{@env['spdy'].momentum_request.spdy_info[:stream_id]}] app processing took #{(Time.now - @process_start).to_f} secs"
         r
+      end
+
+      def prepare!
+        old_delegate = @env['spdy']
+        push_queue = EM::Queue.new
+
+        push_queue.pop do |push_url|
+          old_delegate.push(push_url)
+        end
+
+        @env['spdy'] = Momentum::AppDelegate.new(@req) do |what, push_url|
+          case what
+          when :push
+            push_queue.push(push_url)
+          else
+            raise "Unknown callback #{what}"
+          end
+        end
       end
       
       def send_response(response)
