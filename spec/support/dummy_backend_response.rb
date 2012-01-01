bcf4ecc16c7defb5b91c0824830d4f4b81cd18c3
Jonas Schneider <mail@jonasschneider.com>
2012-01-01 13:52:31 +0100

    allow a body to be passed to DummyBackendResponse

--- spec/support/dummy_backend_response.rb
+++ spec/support/dummy_backend_response.rb
@@ -1,10 +1,11 @@
 class DummyBackendResponse < Momentum::Backend::Response
-  def initialize(options)
+  def initialize(options = {})
     @options = options
   end
   
   def dispatch!
     @on_headers.call(@options[:headers] || {})
+    @on_body.call(@options[:body] || '')
     @on_complete.call
   end
 end
