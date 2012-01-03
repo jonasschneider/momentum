6cc8b2905fa9990bdf5240105fc38f01c4ffbffe
Jonas Schneider <mail@jonasschneider.com>
2012-01-03 16:22:01 +0100

    more spec refactoring

--- spec/momentum/request_stream_spec.rb
+++ spec/momentum/request_stream_spec.rb
@@ -5,14 +5,15 @@ require File.expand_path("../../support/dummy_backend_response", __FILE__)
 
 # TODO: Avoid push recursion
 # TODO: Proper error handling
+# TODO: change #send_data_frame to #write so the Stream is concerned with chunking
 describe Momentum::RequestStream do
   let(:response_headers) { { 'a' => 'b' } }
   let(:response_body) { 'test' }
   let(:backend_response) { DummyBackendResponse.new(:headers => response_headers, :body => response_body) }
   let(:request_headers) { { 'method' => 'get', 'version' => 'HTTP/1.1', 'url' => '/', 'host' => 'localhost', 'scheme' => 'http' } }
 
-  let(:backend) { stub(:prepare => backend_response) }
-  let(:session) { stub(:logger => stub(:debug => true, :info => true)) }
+  let(:backend) { double(:prepare => backend_response) }
+  let(:session) { double('Session', :logger => double(:debug => true, :info => true)) }
   let(:stream) { described_class.new(1, session, request_headers, backend) }
 
   it "sends back the response headers & body" do
@@ -20,6 +21,46 @@ describe Momentum::RequestStream do
     session.should_receive(:send_data_frame).with(1, response_body, false)
     session.should_receive(:send_fin).with(1)
     stream.process_request!
+  end
+
+  context "Responses larger than the chunk size" do
+    let(:chunk_size) { Momentum::Stream::CHUNK_SIZE }
+    let(:backend_response) { DummyBackendResponse.new(:headers => response_headers, :body => 'x'*chunk_size*3) }
+
+    it "sends the response in multiple data frames" do
+      pending
+      session.should_receive(:send_syn_reply).with(1, response_headers)
+      times_called = 0
+      session.should_receive(:send_data_frame) do
+        times_called += 1
+      end
+      session.should_receive(:send_fin) do
+        EM.stop
+      end
+      Timeout.timeout(1) do
+        EM.run do
+          stream.process_request!
+        end
+      end
+      times_called.should == 3
+    end
+  end
+
+  context "Request headers" do
+    let(:session) { double('Session').as_null_object }
+
+    it "passes request headers" do
+      backend = Object.new
+      backend.stub(:prepare) do |req|
+        req.headers.should == request_headers
+
+        DummyBackendResponse.new(:headers => response_headers)
+      end
+
+      session.should_receive(:send_syn_reply).with(1, response_headers)
+
+      stream.process_request!
+    end
   end
 
   context "Server Push" do
@@ -48,37 +89,5 @@ describe Momentum::RequestStream do
 
       stream.process_request!
     end
-  end
-
-  let(:response_test) { 'asdf' }
-
-
-  it "chunks up long responses" do
-    pending
-    one_chunk = 4096
-    app = lambda { |env| [200, {"Content-Type" => "text/plain"}, ['x'*one_chunk*3]] }
-
-    EM.run do
-      Momentum.start(Momentum::Backend.new(app))
-      EventMachine::connect 'localhost', 5555, DumbSPDYClient
-    end
-
-    DumbSPDYClient.body_chunk_count.should == 3
-  end
-
-  it "passes request & response headers" do
-    pending
-    backend = Object.new
-    backend.stub(:prepare) do |req|
-      req.headers['accept-encoding'].should == 'gzip,deflate,sdch'
-      DummyBackendResponse.new(:headers => {'a' => 'b'})
-    end
-
-    EM.run do
-      Momentum.start(backend)
-      EventMachine::connect 'localhost', 5555, DumbSPDYClient
-    end
-
-    DumbSPDYClient.headers['a'].should == 'b'
   end
 end
