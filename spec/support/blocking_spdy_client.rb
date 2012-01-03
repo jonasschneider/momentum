c3ead763e7016dc14aadee00281ad2d93a35c83b
Jonas Schneider <mail@jonasschneider.com>
2012-01-04 00:21:34 +0100

    make the client suck less

--- spec/support/blocking_spdy_client.rb
+++ spec/support/blocking_spdy_client.rb
@@ -5,16 +5,16 @@ class BlockingSPDYClient
     @sock = EventMachine::Synchrony::TCPSocket.new host, port
     @zlib = SPDY::Zlib.new
   end
-  
+
   def request(url)
     sr = SPDY::Protocol::Control::SynStream.new({:zlib_session => @zlib})
-    
+
     headers = {
       "accept"=>"text/html", "host"=>"127.0.0.1:9000",
       "method"=>"GET", "scheme"=>"http",
       "url"=>url,"version"=>"HTTP/1.1"
     }
-    
+
     send_packet sr.create({:stream_id => 1, :headers => headers})
   end
 
@@ -23,14 +23,16 @@ class BlockingSPDYClient
   end
 
   def read_packet
-    buf = read(1)
+    buf = read(8)
     type = buf[0,1].unpack('C').first >> 7 & 0x01
+
+    length_data = buf[5,3].unpack('nc')
+    len = (length_data[0] << 8) + length_data[1]
+    buf << read(len) if len > 0
+
     case type
       when CONTROL_BIT then
-        buf << read(11)
-        pckt = Control::Header.new.read(buf)
-        buf << read(pckt.len-4) # FIXME: wtf?
-        case pckt.type.to_i
+        case ctype = buf[2,2].unpack('n').first
           when 1 then # SYN_STREAM
             pckt = Control::SynStream.new({:zlib_session => @zlib})
             pckt.parse(buf)
@@ -38,17 +40,19 @@ class BlockingSPDYClient
           when 2 then # SYN_REPLY
             pckt = Control::SynReply.new({:zlib_session => @zlib})
             pckt.parse(buf)
-            
+
           when 6 then # PING
             pckt = Control::Ping.new
             pckt.read(buf)
+
+          when 8 then # HEADERS
+            pckt = Control::Headers.new({:zlib_session => @zlib_session})
+            pckt.parse(buf)
+
+          else
+            raise "unknown control frame type #{ctype}"
         end
       when DATA_BIT then
-        buf << read(7)
-        data = buf[5,3].unpack('nc')
-        len = (data[0] << 8) + data[1]
-        buf << read(len) if len > 0
-        
         pckt = Data::Frame.new.read(buf)
         pckt.read(buf)
       else
@@ -56,14 +60,18 @@ class BlockingSPDYClient
     end
     pckt
   end
-  
+
   def read(len)
-    x = @sock.read(len)
-    #puts x.inspect
+    x = @sock.read(len).force_encoding('ASCII-8BIT')
+    raise "read fail" unless x.bytesize == len
     x
   end
-  
+
   def receive_data data
     @parser << data
+  end
+
+  def close
+    @sock.close
   end
 end
