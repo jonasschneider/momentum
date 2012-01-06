5f93189a49a0d038a58c6af88918c521c91a00c3
Jonas Schneider <mail@jonasschneider.com>
2012-01-07 00:05:19 +0100

    set an X-Forwarded-For header for proxied requests

--- lib/momentum/adapters/proxy.rb
+++ lib/momentum/adapters/proxy.rb
@@ -3,13 +3,13 @@ module Momentum
     class Proxy
       class Body
         include EventMachine::Deferrable
-        
+
         def call(body)
           body.each do |chunk|
             @body_callback.call(chunk)
           end
         end
-      
+
         def each &blk
           @body_callback = blk
         end
@@ -18,36 +18,40 @@ module Momentum
       def initialize(host, port)
         @host, @port = host, port
       end
-      
+
       def call(env)
         dup._call(env)
       end
-      
+
       def _call(env)
         req = env['spdy'].momentum_request
-        
+
         url = req.uri.dup
         url.host = @host
         url.port = @port
 
-        http = EventMachine::HttpRequest.new(url).send(req.headers['method'], :head => req.headers, :body => req.body)
+        headers = req.headers
+
+        headers['X-Forwarded-For'] = req.spdy_info[:remote_addr]
+
+        http = EventMachine::HttpRequest.new(url).send(req.headers['method'], :head => headers, :body => req.body)
         body = Body.new
-        
+
         http.headers do |headers|
           headers['status'] = headers.http_status
           headers['version'] = headers.http_version
-          
+
           env['async.callback'].call [headers.http_status, headers, body]
         end
-        
+
         http.stream do |chunk|
           body.call [chunk]
         end
-        
+
         http.callback do
           body.succeed
         end
-        
+
         throw(:async)
       end
     end
--- spec/momentum/adapters/proxy_spec.rb
+++ spec/momentum/adapters/proxy_spec.rb
@@ -24,6 +24,32 @@ describe Momentum::Adapters::Proxy do
     Momentum::Backend.new(app)
   end
 
+  # see http://unicorn.bogomips.org/examples/nginx.conf
+  context 'X-Forwarded-For header' do
+    let(:client_address) { '123.231.132.0' }
+    let(:request_headers) { { 'method' => 'post', 'version' => 'HTTP/1.1', 'url' => '/', 'host' => 'localhost', 'scheme' => 'http' } }
+    let(:expected_upstream_headers) { request_headers.merge({ 'X-Forwarded-For' => client_address}) }
+
+    it "gets set the to request.spdy_info[:remote_addr]" do
+      stub_request(:post, "http://localhost:5556/").
+        with(:headers => expected_upstream_headers).
+        to_return(:status => 200, :body => 'done', :headers => {})
+
+      request = Momentum::Request.new(:headers => request_headers)
+      request.spdy_info[:remote_addr] = client_address
+
+      response = backend.prepare(request)
+      response.on_body do |data|
+        data.should == 'done'
+      end
+
+      EM.run do
+        response.dispatch!
+        EM.stop
+      end
+    end
+  end
+
   it "passes the request body on" do
     stub_request(:post, "http://localhost:5556/").
       with(:body => 'ohai').
